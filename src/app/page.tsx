37a5eeff6ead6a4e772350196807c24f39bd1baa
Sourav Layek <souravlayek11@gmail.com>
2024-10-26 18:39:03 +0530

    Updated hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import { FlipWords } from "@/components/ui/flip-words";
 import React from "react";
 
@@ -11,15 +12,20 @@ const words: string[] = [
 ];
 const HomePage = () => {
   return (
-    <main className="overflow-hidden">
+    <main>
       <section className="h-[60vh] flex items-center">
-        <div className="container text-xl sm:text-2xl md:text-3xl lg:text-4xl font-normal text-neutral-600 dark:text-neutral-400">
-          <div>
-            I’m Sourav, Your Go-To for
-            <FlipWords words={words} />
-            <span className="inline md:block">Digital Products</span>
+        <BackgroundBeamsWithCollision>
+          <div className="container">
+            <p className="text-lg font-medium text-neutral-600 dark:text-neutral-400">
+              Hey there,
+            </p>
+            <h1 className=" text-xl sm:text-2xl md:text-3xl lg:text-4xl font-normal text-neutral-600 dark:text-neutral-400">
+              I’m Sourav, Your Go-To for
+              <FlipWords words={words} />
+              <span className="inline md:block">Digital Products</span>
+            </h1>
           </div>
-        </div>
+        </BackgroundBeamsWithCollision>
       </section>
     </main>
   );
